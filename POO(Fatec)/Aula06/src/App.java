70a9be1365afb711946f28a445f5a451d7647e7c
Igor Oliveira <igornutrietec2020@gmail.com>
2024-03-27 10:59:24 -0300

    Aula06
    Na aula 06 foi realizada uma atividade onde fizemos um código bem simples de Java onde apenas demos um print com todas as nossas informações

---
+++ POO(Fatec)/Aula06/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void funcao_teste (){
+        
+    }
+    public static void main(String[] args) throws Exception {
+        funcao_teste();
+        System.out.println("Nome: Igor Oliveira da Silva");
+        System.out.println("RGM: 2920482321042");
+        System.out.println("Idade: 19 anos");
+        System.out.println("GitHub: @igorodsilva");
+        System.out.println("Linkedin: @igorodsilva");
+        System.out.println("Curso e Turma: ADS - Segundo Semestre");
+    }
+
+}
+
+
+
